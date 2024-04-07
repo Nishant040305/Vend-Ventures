d77bb39f592b081e0c80f74b34381d72f89a4ac4
Nishant <nishant040305@gmail.com>
2024-04-08 03:15:01 +0530

    login and main page structure implemented

---
+++ server/models/mongodb.js
@@ -0,0 +1,12 @@
+// Connect to MongoDB
+const mongoose = require('mongoose');
+
+connectToMonogo().catch(err => console.log(err));
+
+async function connectToMonogo() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/vendVentures');
+  console.log("connected to database");
+
+}
+
+module.exports=connectToMonogo;
---
+++ server/models/userSchema.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const {Schema} = mongoose;
+const userSchema = new Schema(
+    {
+        googleId:{
+            type:String,
+            required:true
+        },
+        displayName:{
+            type:String,
+            required:true,
+        },
+        email:{
+            type:String
+        },
+        image:{
+            type:String
+        }
+    }
+)
+
+const user = mongoose.model('user',userSchema);
+user.createIndexes();
+module.exports = user;
