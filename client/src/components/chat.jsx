697a406858cd6414fd481be08c5e6493d8dc9fa8
Aryan10 <aryan.shivam.10@gmail.com>
2024-04-12 22:49:11 +0530

    CHAT feature

--- client/src/components/chat.jsx
+++ client/src/components/chat.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import './chat.css';
 import axios from 'axios';
 import Navbar from './Navbar';
@@ -10,7 +10,7 @@ const Message =(props)=>{
         <div className="message" style={{float:props.sender}}>
 
             {props.sender==="left"?<div className="message-sticker-chat rounded-circle"><img className="sticker-chat " src="/user_.png" ></img></div>:<div/>}
-            <div className="message-text">asfkljhirufdnmhfjkfsjdfnkr;hsddddddddddddddddddddddddddnmsffffffffffffffffffffffffffffffffddddddddddddddddddddddlskddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd</div>
+            <div className="message-text">{props.text}</div>
             {props.sender==="right"?<div className="message-sticker-chat rounded-circle"><img className="sticker-chat " src="/user_.png" ></img></div>:<div/>}
 
         </div>
@@ -19,22 +19,85 @@ const Message =(props)=>{
 const Chats = ()=>{
     const {chatId} = useParams();
     const {state} = useLocation();
-    const {user, owner, channel, isOwner} = state;
+    const [messeges,setMessages] = useState([]);
+    const [message_sending,setMess] = useState();
+    const [Reciever, setRec] = useState(null);
+    const {user, owner, channel} = state;
     // console.log(state);
 
     socket.connect();
-
-    socket.on('message', (message) => {
-        console.log(message);
-    });
-
-    function sendMessage(value) {
+    useEffect(() => {
+        fetchMessages();
+        socket.on('message', (msg) => {
+            setMessages(prevMessages => [...prevMessages, {
+                content: msg.message.content,
+                sender: ((msg.user === user._id) ? "right" : "left")
+            }]);
+        });
+        return () => {
+            socket.off('message');
+        };
+    }, []);
+    const handleKeyPress = (event) => {
+        if (event.key === "Enter") {
+          sendMessage();
+        }
+      };
+    const handleChange =(e)=>{
+        setMess(e.target.value);
+    }
+    function sendMessage() {
         var msg = {
             channel: chatId,
-            user: isOwner ? owner._id : user._id,
-            message: { type: "text", content: value || "Hello World" }
+            user: user._id || "Dummy User",
+            message: { type: "text", content: message_sending}
         }
-        socket.emit('message', msg);
+        if (message_sending) socket.emit('message', msg);
+        setMess("");
+    }
+    async function fetchMessages() {
+        // user display name
+        try {
+            const response = await axios.post('http://localhost:5000/userinfo/', {
+                _id: channel.userId
+            }, {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+
+            if (response.data.error) {
+                throw new Error(response.data.error);
+            } else {
+                console.log(response.data);
+                setRec(response.data.data);
+            }
+        } catch (error) {
+            console.log(error.message);
+        }
+        // messages
+        try {
+            const response = await axios.post('http://localhost:5000/fetchChat/', {
+                _id: chatId
+            }, {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+
+            if (response.data.error) {
+                throw new Error(response.data.error);
+            } else {
+                setMessages(response.data.channel.messages.map(function(i) {
+                    return {
+                        content: i.message.content,
+                        sender: ((i.user === user._id) ? "right" : "left")
+                    }
+                }));
+            }
+        } catch (error) {
+            console.log(error.message);
+        }
     }
 
     return(
@@ -43,39 +106,20 @@ const Chats = ()=>{
             <div className="chat-collection">
                 <div className="main-info">
                     <img src="/images/img1.jpg" className="rounded-circle main-info-chat"></img> 
-                    <div class="users-info">
-                    <div className="whitedata"><img src="/user_.png" className="sticker-chat"></img>&nbsp;Nishant Mohan</div>
-                    <div className="whitedata"><img src="/user_.png" className="sticker-chat"></img>&nbsp;Nishant Mohan</div>
-                    <div className="whitedata"><img src="/user_.png" className="sticker-chat"></img>&nbsp;Nishant Mohan</div>
-                    <div className="whitedata"><img src="/user_.png" className="sticker-chat"></img>&nbsp;Nishant Mohan</div>
-                    <div className="whitedata"><img src="/user_.png" className="sticker-chat"></img>&nbsp;Nishant Mohan</div>
-                    <div className="whitedata"><img src="/user_.png" className="sticker-chat"></img>&nbsp;Nishant Mohan</div>
-                    <div className="whitedata"><img src="/user_.png" className="sticker-chat"></img>&nbsp;Nishant Mohan</div>
-                    <div className="whitedata"><img src="/user_.png" className="sticker-chat"></img>&nbsp;Nishant Mohan</div>
-                    <div className="whitedata"><img src="/user_.png" className="sticker-chat"></img>&nbsp;Nishant Mohan</div>
-                    <div className="whitedata"><img src="/user_.png" className="sticker-chat"></img>&nbsp;Nishant Mohan</div>
-                    <div className="whitedata"><img src="/user_.png" className="sticker-chat"></img>&nbsp;Nishant Mohan</div>
-                    <div className="whitedata"><img src="/user_.png" className="sticker-chat"></img>&nbsp;Nishant Mohan</div>
-                    <div className="whitedata"><img src="/user_.png" className="sticker-chat"></img>&nbsp;Nishant Mohan</div>
-                    <div className="whitedata"><img src="/user_.png" className="sticker-chat"></img>&nbsp;Nishant Mohan</div>
-                    <div className="whitedata"><img src="/user_.png" className="sticker-chat"></img>&nbsp;Nishant Mohan</div>
-                    <div className="whitedata"><img src="/user_.png" className="sticker-chat"></img>&nbsp;Nishant Mohan</div>
-                    <div className="whitedata"><img src="/user_.png" className="sticker-chat"></img>&nbsp;Nishant Mohan</div>
-                    <div className="whitedata"><img src="/user_.png" className="sticker-chat"></img>&nbsp;Nishant Mohan</div>
-                    <div className="whitedata"><img src="/user_.png" className="sticker-chat"></img>&nbsp;Nishant Mohan</div>
-                    <div className="whitedata"><img src="/user_.png" className="sticker-chat"></img>&nbsp;Nishant Mohan</div>
-                    <div className="whitedata"><img src="/user_.png" className="sticker-chat"></img>&nbsp;Nishant Mohan</div>
-                    <div className="whitedata"><img src="/user_.png" className="sticker-chat"></img>&nbsp;Nishant Mohan</div>
-
-                    </div>
+                    {Reciever?<div class="users-info">
+                    <div className="whitedata"><img src="/user_.png" className="sticker-chat"></img>&nbsp;{owner.displayName}</div>
+                    <div className="whitedata"><img src="/user_.png" className="sticker-chat"></img>&nbsp;{Reciever.displayName}</div>
+                    </div>:<div></div>}
                     </div>
                 <div className="chatting">
-                    <div className="messging">
-                        <Message sender="right"></Message>
-                        <Message sender="left"></Message>
-
+                    <div className="messging" id = "dashboard_message">
+                    {
+                    messeges.map((info, index) => (
+                        <Message sender={info.sender} text={info.content}></Message>
+                    ))
+                    }
                     </div>
-                    <div className="message-chat"><input className="message-box"></input><div><button onClick={()=>{sendMessage()}} className="btn btn-success">Send</button></div></div>
+                    <div className="message-chat"><input className="message-box" value={message_sending} onChange={handleChange} onKeyDown={handleKeyPress}></input><div><button onClick={()=>{sendMessage()}} className="btn btn-success">Send</button></div></div>
                 </div>
             </div>
             <Footer></Footer>
--- client/src/socket-App.test.js
+++ client/src/socket-App.test.js
@@ -1,84 +1,84 @@
-// import logo from './logo.svg';
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import Mainpage from './components/mainpage';
-import Product from './components/product';
-import Form from './components/productForm/postAdd';
-import MobilePost from './components/productForm/mobilePost';
-import Error from './components/error404';
-import CarPost from './components/productForm/carPost';
-import RealState from './components/productForm/realState';
-import Electronics from './components/productForm/electorics';
-import MotorCycle from './components/productForm/motorCycle';
+// // import logo from './logo.svg';
+// import './App.css';
+// import { Route, Routes } from 'react-router-dom';
+// import Mainpage from './components/mainpage';
+// import Product from './components/product';
+// import Form from './components/productForm/postAdd';
+// import MobilePost from './components/productForm/mobilePost';
+// import Error from './components/error404';
+// import CarPost from './components/productForm/carPost';
+// import RealState from './components/productForm/realState';
+// import Electronics from './components/productForm/electorics';
+// import MotorCycle from './components/productForm/motorCycle';
 
-import Cart from './components/cart';
-import Chats from './components/chat'
+// import Cart from './components/cart';
+// import Chats from './components/chat'
 
 
-import React, { useState, useEffect } from 'react';
-import { socket } from './socket';
-import { ConnectionState } from './components/socketTest/ConnectionState';
-import { ConnectionManager } from './components/socketTest/ConnectionManager';
-import { Events } from "./components/socketTest/Events";
-import { MyForm } from './components/socketTest/MyForm';
+// import React, { useState, useEffect } from 'react';
+// import { socket } from './socket';
+// import { ConnectionState } from './components/socketTest/ConnectionState';
+// import { ConnectionManager } from './components/socketTest/ConnectionManager';
+// import { Events } from "./components/socketTest/Events";
+// import { MyForm } from './components/socketTest/MyForm';
 
-function App() {
-  // SOCKET.IO TEST
-  const [isConnected, setIsConnected] = useState(socket.connected);
-  const [events, setEvents] = useState([]);
+// function App() {
+//   // SOCKET.IO TEST
+//   const [isConnected, setIsConnected] = useState(socket.connected);
+//   const [events, setEvents] = useState([]);
 
-  useEffect(() => {
-    function onConnect() {
-      setIsConnected(true);
-    }
-    function onDisconnect() {
-      setIsConnected(false);
-    }
-    function onMessage(value) {
-      setEvents(previous => [...previous, `[${value.user}] ${value.message.content}`]);
-    }
+//   useEffect(() => {
+//     function onConnect() {
+//       setIsConnected(true);
+//     }
+//     function onDisconnect() {
+//       setIsConnected(false);
+//     }
+//     function onMessage(value) {
+//       setEvents(previous => [...previous, `[${value.user}] ${value.message.content}`]);
+//     }
 
-    socket.on('connect', onConnect);
-    socket.on('disconnect', onDisconnect);
-    socket.on('message', onMessage);
+//     socket.on('connect', onConnect);
+//     socket.on('disconnect', onDisconnect);
+//     socket.on('message', onMessage);
 
-    return () => {
-      socket.off('connect', onConnect);
-      socket.off('disconnect', onDisconnect);
-      socket.off('message', onMessage);
-    };
-  }, []);
+//     return () => {
+//       socket.off('connect', onConnect);
+//       socket.off('disconnect', onDisconnect);
+//       socket.off('message', onMessage);
+//     };
+//   }, []);
 
-  return(
-  <div className='App'>
+//   return(
+//   <div className='App'>
 
-      {/* SOCKET.IO DEBUG */}
-      <ConnectionState isConnected={ isConnected } />
-      <Events events={ events } />
-      <ConnectionManager />
-      <MyForm />
+//       {/* SOCKET.IO DEBUG */}
+//       <ConnectionState isConnected={ isConnected } />
+//       <Events events={ events } />
+//       <ConnectionManager />
+//       <MyForm />
 
-      <Routes>
-          <Route path="/:_" element = {<Mainpage></Mainpage>}></Route>
-          <Route path="/cart" element={<Cart></Cart>}></Route>
-          <Route path="/chat/:chatId" element={<Chats></Chats>}></Route>
+//       <Routes>
+//           <Route path="/:_" element = {<Mainpage></Mainpage>}></Route>
+//           <Route path="/cart" element={<Cart></Cart>}></Route>
+//           <Route path="/chat/:chatId" element={<Chats></Chats>}></Route>
 
-        <Route path="/" element = {<Mainpage></Mainpage>}></Route>
+//         <Route path="/" element = {<Mainpage></Mainpage>}></Route>
 
-        <Route path="/product/:productID" element = {<Product></Product>}></Route>
-        <Route path="/post/mobile" element={<MobilePost></MobilePost>}></Route>
-        <Route path="/error" element={<Error></Error>}></Route>
-        <Route path="/post/car" element={<CarPost></CarPost>}></Route>
-        <Route path="post/realstate" element = {<RealState></RealState>}></Route>
-        <Route path="post/electronics" element = {<Electronics></Electronics>}></Route>
-        <Route path="post/motorcycle" element={<MotorCycle></MotorCycle>}></Route>
-        <Route path="post" element={<Form></Form>}/>
-        <Route path="*" element={<Mainpage></Mainpage>}></Route>
+//         <Route path="/product/:productID" element = {<Product></Product>}></Route>
+//         <Route path="/post/mobile" element={<MobilePost></MobilePost>}></Route>
+//         <Route path="/error" element={<Error></Error>}></Route>
+//         <Route path="/post/car" element={<CarPost></CarPost>}></Route>
+//         <Route path="post/realstate" element = {<RealState></RealState>}></Route>
+//         <Route path="post/electronics" element = {<Electronics></Electronics>}></Route>
+//         <Route path="post/motorcycle" element={<MotorCycle></MotorCycle>}></Route>
+//         <Route path="post" element={<Form></Form>}/>
+//         <Route path="*" element={<Mainpage></Mainpage>}></Route>
 
-      </Routes>
-      {/* <Mainpage></Mainpage> */}
-  </div>
-  );
-}
+//       </Routes>
+//       {/* <Mainpage></Mainpage> */}
+//   </div>
+//   );
+// }
 
-export default App;
+// export default App;
